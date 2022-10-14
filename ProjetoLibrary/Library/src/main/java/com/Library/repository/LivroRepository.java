fd61e88a5ddfe95b43d5de9e0d45b16e6f282a5c
Bruna Cristina Andrade <38590890+buandradels@users.noreply.github.com>
2022-10-14 20:05:17 -0300

    Add files via upload

---
+++ ProjetoLibrary/Library/src/main/java/com/Library/repository/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.Library.repository;
+
+import com.Library.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+}
